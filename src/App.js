148f390da63cb2aac3c9d2ffbb9c7917ee112b06
Aditya Bhardwaj <vishubhardwaj321@gmail.com>
2021-09-09 22:34:36 +0530

    Updated navbar.js, App,js and index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css'
 
 function App(){
     return (
-        <div className='App'>
+        <>
+        <Router>
             <Navbar />
-        </div>
+            <switch>
+                <Route path='/' exact/>
+            </switch>
+        </Router>    
+        </>
     )
 }
 
-export default App;
+export default App
